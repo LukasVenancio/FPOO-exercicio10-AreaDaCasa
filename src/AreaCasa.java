099d77254d927cce1768fa50aa424f33655a55e8
LUKAS SANTOS VENANCIO <21276375@cfp127aluno.local>
2021-09-09 13:06:08 -0300

    Algoritimo finalizado.

--- src/AreaCasa.java
+++ src/AreaCasa.java
@@ -7,32 +7,30 @@ public class AreaCasa {
 		Scanner leitor = new Scanner(System.in);
 
 		String nome, desejo = "S";
-		double largura, comprimento, areaComodo = 0, areaTotal;
-		
-		
-		while(desejo.equals("S") || desejo.equals("s")) {
-		
-		System.out.println("Qual é o nome do cômodo?");
-		nome = leitor.next();
-		System.out.println("Qual a largura?");
-		largura = leitor.nextDouble();
-		System.out.println("Qual o comprimento?");
-		comprimento = leitor.nextDouble();
-		
-		areaComodo = largura * comprimento;
-		
-		
-		System.out.println("A largura do(a) " + nome + " é: " + areaComodo);
-		
-		System.out.println("Você deseja incluir mais cômodos?");
-		desejo = leitor.next();
-		System.out.println();
-	 areaTotal = areaComodo + areaComodo;
+		double largura, comprimento, areaComodo = 0, areaTotal = 0;
+
+		while (desejo.equals("S") || desejo.equals("s")) {
+
+			System.out.println("Qual é o nome do cômodo?");
+			nome = leitor.next();
+			System.out.println("Qual a largura?");
+			largura = leitor.nextDouble();
+			System.out.println("Qual o comprimento?");
+			comprimento = leitor.nextDouble();
+
+			areaComodo = largura * comprimento;
+
+			System.out.println("A largura do(a) " + nome + " é: " + areaComodo + " metros quadrados.");
+
+			System.out.println("Você deseja incluir mais cômodos? (S ou N)");
+			desejo = leitor.next();
+			System.out.println();
+			areaTotal = areaComodo + areaTotal;
 		}
-		
-		
-		System.out.println(areaComodo);
-	
+		leitor.close();
+
+		System.out.println("-------------------------------------------------------------------");
+		System.out.println("A área total dos cômodos listados é: " + areaTotal + " metros quadrados.");
 	}
 
 }
